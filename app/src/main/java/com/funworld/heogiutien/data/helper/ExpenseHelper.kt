ae6555df5fbe79513956fb52bf51dd06c9d481f2
Bin <thanhtd1987@gmail.com>
2018-01-29 01:14:09 +0700

    [GENERAL] add Helper for expense

---
+++ app/src/main/java/com/funworld/heogiutien/data/helper/ExpenseHelper.kt
@@ -0,0 +1,22 @@
+package com.funworld.heogiutien.data.helper
+
+import com.funworld.heogiutien.data.dao.Expense
+import com.funworld.heogiutien.data.dao.Resource
+
+class ExpenseHelper{
+
+    companion object {
+        fun quickAddExpense(purpose: String, amount: Int, resource: Resource, relatedType: String,
+                            relatedPerson: String, note: String): Expense{
+            val expense = Expense()
+            expense.purpose = purpose
+            expense.amount = amount
+            expense.resourceId = resource
+            expense.relatedType = relatedType
+            expense.relatedPerson = relatedPerson
+            expense.note = note
+            expense.save()
+            return expense
+        }
+    }
+}
