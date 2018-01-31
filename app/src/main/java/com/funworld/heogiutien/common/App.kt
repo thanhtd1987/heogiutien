a452b9169fe4338733fd8bf883cbfc4ebaf2a1f2
Bin <thanhtd1987@gmail.com>
2018-02-01 05:13:34 +0700

    [DB] config DB 's tables, add rows of 'Resource' table

--- app/src/main/java/com/funworld/heogiutien/common/App.kt
+++ app/src/main/java/com/funworld/heogiutien/common/App.kt
@@ -1,9 +1,20 @@
 package com.funworld.heogiutien.common
 
 import com.activeandroid.app.Application
+import com.funworld.heogiutien.data.helper.ResourceHelper
 
 /**
  * Created by ThanhTD on 6/25/2017.
  */
 
-class App : Application()
+class App : Application(){
+    override fun onCreate() {
+        super.onCreate()
+        initResourceDB()
+    }
+
+    private fun initResourceDB(){
+        ResourceHelper.addResource("CASH", "chi tieu tien mat")
+//        ResourceHelper.addResource("CASH", "chi tieu tien mat")
+    }
+}
--- app/src/main/java/com/funworld/heogiutien/data/dao/Resource.kt
+++ app/src/main/java/com/funworld/heogiutien/data/dao/Resource.kt
@@ -3,6 +3,7 @@ package com.funworld.heogiutien.data.dao
 import com.activeandroid.Model
 import com.activeandroid.annotation.Column
 import com.activeandroid.annotation.Table
+import com.activeandroid.query.Select
 
 
 /**
@@ -10,31 +11,51 @@ import com.activeandroid.annotation.Table
  */
 
 @Table(name = "Resource")
-class Resource : Model() {
+class Resource() : Model() {
 
-//    @Column(name = "resource_id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
+    //    @Column(name = "resource_id", unique = true, onUniqueConflict = Column.ConflictAction.REPLACE)
 //    var resourceId: Int = 0
     //ten cua nguon tien
     @Column(name = "name")
     lateinit var name: String
     // mo ta ve nguon tien
-    @Column(name="description")
-    lateinit var description : String
+    @Column(name = "description")
+    lateinit var description: String
     // so du dau ki ( thong thuong la thang)
-    @Column(name="opening_balance")
-    var openingBalance : Int = 0
+    @Column(name = "opening_balance")
+    var openingBalance: Int = 0
+    @Column(name = "opening_date")
+    var openingDate: Long = 0
+
     // so du cuoi ki ( thong thuong la thang)
-    @Column(name="closing_balance")
-    var closingBalance : Int = 0
+    @Column(name = "closing_balance")
+    var closingBalance: Int = 0
+    @Column(name = "closing_date")
+    var closingDate: Long = 0
+
     // so du hien tai
-    @Column(name="current_balance")
-    var currentBalance : Int = 0
+    @Column(name = "current_balance")
+    var currentBalance: Int = 0
     // chi tieu cuoi cung
-    @Column(name="last_expense")
-    lateinit var lastExpense : Expense
+    @Column(name = "last_expense")
+    var lastExpense: Expense? = null
+
+    constructor(name: String, description: String) : this() {
+        this.name = name
+        this.description = description
+    }
 
     fun expenses(): MutableList<Expense> {
         return getMany(Expense::class.java, "resource")
     }
 
+
+    companion object {
+        fun getResourceByName(name: String): Resource {
+            return Select().from(Resource::class.java)
+                    .where("name = ?", name)
+                    .executeSingle()
+        }
+
+    }
 }
