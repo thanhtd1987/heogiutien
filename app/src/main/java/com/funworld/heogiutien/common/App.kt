57a860c49939f6f024e5e066f11a0c97671f380a
Bin <thanhtd1987@gmail.com>
2018-02-03 17:16:08 +0700

    [UI] add splash screen + move adding DB to this state

--- app/src/main/java/com/funworld/heogiutien/common/App.kt
+++ app/src/main/java/com/funworld/heogiutien/common/App.kt
@@ -1,20 +1,9 @@
 package com.funworld.heogiutien.common
 
 import com.activeandroid.app.Application
-import com.funworld.heogiutien.data.helper.ResourceHelper
 
 /**
  * Created by ThanhTD on 6/25/2017.
  */
 
-class App : Application(){
-    override fun onCreate() {
-        super.onCreate()
-        initResourceDB()
-    }
-
-    private fun initResourceDB(){
-        ResourceHelper.addResource("CASH", "chi tieu tien mat")
-//        ResourceHelper.addResource("CASH", "chi tieu tien mat")
-    }
-}
+class App : Application()
---
+++ app/src/main/java/com/funworld/heogiutien/features/splash/SplashActivity.kt
@@ -0,0 +1,35 @@
+package com.funworld.heogiutien.features.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.support.v7.app.AppCompatActivity
+import com.funworld.heogiutien.R
+import com.funworld.heogiutien.data.helper.ResourceHelper
+import com.funworld.heogiutien.features.main.MainActivity
+import org.joda.time.DateTime
+
+class SplashActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val currentTime = DateTime.now().millis
+        val endTime = initResourceDB()
+        var time = endTime - currentTime
+        if(time > 1000) time = 1000
+
+        Handler().postDelayed(Runnable {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }, 1000 - time)
+    }
+
+    private fun initResourceDB(): Long{
+//        ResourceHelper.deleteByName("CASH")
+        ResourceHelper.addResource("CASH", "chi tieu tien mat")
+//        ResourceHelper.addResource("CASH", "chi tieu tien mat")
+        return DateTime.now().millis
+    }
+}
